ca6ed8a564e29c28bc644630fbf5a2626ed1b8a9
Godwin Oluwadipe <oluwadipegodwin@gmail.com>
2026-04-24 20:42:29 +0100

    fix ContactNotificationEmail import path issue

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import clientPromise from "@/lib/mongodb-client";
-import { ContactNotificationEmail } from "../email/ContactNotification";
-
+import { ContactNotificationEmail } from "@/app/email/ContactNotification";
 // Initialize Resend - Ensure RESEND_API_KEY is in your .env file
 const resend = new Resend(
 	process.env.RESEND_API_KEY,
